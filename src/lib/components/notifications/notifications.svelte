4d41626013e7373837272c7c202deb95957302a4
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-13 08:12:57 -0500

    Add the component for containing all of the notifications

---
+++ src/lib/components/notifications/notifications.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Notification from "./notification.svelte";
+    import { notifications } from "$lib/stores/notification-store";
+</script>
+
+<div id="notifications-container">
+    {#each $notifications as notification}
+        <Notification {notification} />
+    {/each}
+</div>
+
+<style lang="scss">
+    #notifications-container {
+        position: fixed; top: 1rem;
+        top: 1rem;
+        right: 1rem;
+
+        width: 90%;
+        max-width: 27.5rem;
+    }
+</style>

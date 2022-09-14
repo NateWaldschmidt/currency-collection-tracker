b714552bba350b6a4b70642ece39d9fb6d991365
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-14 07:53:57 -0500

    Add typing for the SvelteKit event.locals object

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,16 @@
 /// <reference types="@sveltejs/kit" />
+import type TokenPayload from '$lib/utilities/auth';
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface Session {}
-	// interface Stuff {}
+declare global {
+	namespace App {
+		interface Locals {
+			user?: TokenPayload | null,
+		}
+		// interface Platform {}
+		// interface Session {}
+		// interface Stuff {}
+	}
 }

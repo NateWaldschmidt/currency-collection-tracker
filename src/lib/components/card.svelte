6bdbc2c8b0d7b31cd25592b6ed9ce1ea5a676f60
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-20 07:47:31 -0500

    Add the Card Svelte component

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    /** The HTML tag for the card. */
+    export let tag: string = "div";
+</script>
+
+<svelte:element this={tag} class="card">
+    <slot></slot>
+</svelte:element>
+
+<style lang="scss">
+    .card {
+        max-width: 14rem;
+        padding: 0.75rem;
+        border-radius: var(--border-radius-lg);
+
+        background-color: var(--color-neutral-light);
+        box-shadow: 2px 2px 25px -18px rgba(133,133,133,1);
+    }
+</style>

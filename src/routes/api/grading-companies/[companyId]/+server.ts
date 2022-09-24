d5284a33d50cc3f9050177012b33c912bd8be29d
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-24 11:30:31 -0500

    Upgrade grading company ID endpoint

--- src/routes/api/grading-companies/[companyId]/+server.ts
+++ src/routes/api/grading-companies/[companyId]/+server.ts
@@ -1,21 +1,33 @@
 import createConnection from "$lib/database/connection";
 import GradingCompanyRepository from "$lib/repository/grading-company-repository";
 import ResponseHelper from "$lib/utilities/response-helper";
-import type { RequestEvent } from "@sveltejs/kit/types/internal";
+import type { RequestHandler } from "@sveltejs/kit/types/internal";
 
-/** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get(event: RequestEvent) {
+/** Finds a particular grading company by ID. */
+export const GET: RequestHandler = async function({params}) {
+    // Ensure the grade ID is an integer.
+    if (!Number.isInteger(+params.companyId)) {
+        return new Response(null, {
+            'status': 400,
+            'statusText': 'The company ID is an invalid form, must be an integer.',
+        });
+    }
+
     /** The passed in coin group ID. */
-    const companyId = Number.parseInt(event.params.companyId);
+    const companyId = Number.parseInt(params.companyId);
 
-    const conn = await createConnection();
-    const gradingCompanyRepo = new GradingCompanyRepository(conn);
-    const gradingCompanies = await gradingCompanyRepo.findById(companyId);
-    conn.end();
+    try {
+        const conn = await createConnection();
+        const gradingCompanyRepo = new GradingCompanyRepository(conn);
+        const gradingCompanies = await gradingCompanyRepo.findById(companyId);
+        conn.end();
 
-    return ResponseHelper.createSuccessResponse(
-        200,
-        `Successfully queried for the grading company with the ID ${companyId}.`,
-        JSON.parse(JSON.stringify(gradingCompanies)),
-    );
+        return ResponseHelper.jsonResponse(
+            `Successfully queried for the grading company with the ID ${companyId}.`,
+            gradingCompanies,
+        );
+    } catch (e) {
+        return ResponseHelper.serverErrorResponse();
+    }
+    
 }

91bef0c31932f24b55b9ac5887e153921a8558dc
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-24 11:32:31 -0500

    Upgrade mints endpoint

--- src/routes/api/mints/+server.ts
+++ src/routes/api/mints/+server.ts
@@ -1,20 +1,21 @@
 import createConnection from "$lib/database/connection";
 import UsMintRepository from "$lib/repository/us-mint-repository";
 import ResponseHelper from "$lib/utilities/response-helper";
+import type { RequestHandler } from "@sveltejs/kit";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get() {
-    /** The database connection. */
-    const conn = await createConnection();
-    /** The repository for interacting with the US mint database table. */
-    const usMintRepo = new UsMintRepository(conn);
-    /** All of the U.S. mints found in the database. */
-    const mints = await usMintRepo.findAll();
-    conn.end();
+export const GET: RequestHandler = async function() {
+    try {
+        const conn = await createConnection();
+        const usMintRepo = new UsMintRepository(conn);
+        const mints = await usMintRepo.findAll();
+        conn.end();
 
-    return ResponseHelper.createSuccessResponse(
-        200,
-        'Successfully queried all U.S mints.',
-        JSON.parse(JSON.stringify(mints)),
-    );
+        return ResponseHelper.jsonResponse(
+            'Successfully queried all U.S mints.',
+            mints,
+        );
+    } catch (e) {
+        return ResponseHelper.serverErrorResponse();
+    }
 }

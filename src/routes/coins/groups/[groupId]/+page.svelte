cf4756e9c8c076239a0bb384496119de7efd0935
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-24 10:11:41 -0500

    Upgrade to the new routing for the coin group route/ endpoint

--- src/routes/coins/groups/[groupId]/+page.svelte
+++ src/routes/coins/groups/[groupId]/+page.svelte
@@ -1,99 +1,86 @@
-<script context="module" lang="ts">
-    import type { Load } from '@sveltejs/kit';
-
-    /** @type {import('./__types/[slug]').Load} */
-    export const load: Load = async function({ params, fetch }) {
-        const coinGroup = await fetch(`/api/coins/groups/${params.groupId}`);
-        const coinGroupJson = await coinGroup.json();
-
-        return {
-            props: {
-                coinGroup: new CoinGroup(coinGroupJson?.data.coinGroup),
-                coins: coinGroupJson?.data.coins.map((coinJson: CoinJson) => new Coin(coinJson)),
-            }
-        };
-    }
-</script>
 <script lang="ts">
+    import Coin from '$lib/models/coin';
+    import type CoinGroup from '$lib/models/coin-group';
+    import Table from '$lib/components/table.svelte'
+    import { heading } from '$lib/stores/page-heading-store';
+    import type { PageData } from './$types';
     import { session } from '$app/stores';
 
-    import BaseLayout from '$lib/layouts/base.svelte';
-    import Coin, { type CoinJson } from '$lib/models/coin';
-    import CoinGroup from '$lib/models/coin-group';
-    import Table from '$lib/components/table.svelte'
+    /** Data loaded into the page. */
+    export let data: PageData;
 
     /** The Groups of Coins. */
-    export let coinGroup: CoinGroup;
+    const coinGroup: CoinGroup = data.coinGroup;
     /** The coins apart of this group. */
-    export let coins: Coin[];
+    const coins: Coin[] = data.coins;
+
+    heading.set(`${coinGroup.title}.`)
 
     /** A way to filter the different coins being displayed by their strike type. */
     let filterStrikeId = [1];
 </script>
 
-<BaseLayout heading={coinGroup.title}>
-    <!-- Strike Filters -->
-    <div class="toggle-controls">
-        <div class="toggle-box">
-            <input type="checkbox" id="filter-business" name="strike-filter" value={1} bind:group={filterStrikeId} />
-            <label for="filter-business">Business</label>
-        </div>
-        <div class="toggle-box">
-            <input type="checkbox" id="filter-proof" name="strike-filter" value={2} bind:group={filterStrikeId} />
-            <label for="filter-proof">Proof</label>
-        </div>
+<!-- Strike Filters -->
+<div class="toggle-controls">
+    <div class="toggle-box">
+        <input type="checkbox" id="filter-business" name="strike-filter" value={1} bind:group={filterStrikeId} />
+        <label for="filter-business">Business</label>
     </div>
+    <div class="toggle-box">
+        <input type="checkbox" id="filter-proof" name="strike-filter" value={2} bind:group={filterStrikeId} />
+        <label for="filter-proof">Proof</label>
+    </div>
+</div>
 
-    <Table
-    pageSizes={[5, 10, 25, 50, 100]}
-    columns={[{
-            title: 'Strike',
-            dataKey: 'strike.title',
-            headingAlignment: 'left',
-            dataAlignment: 'left',
-            sortable: true,
-        }, {
-            title: 'Year',
-            dataKey: 'year',
-            headingAlignment: 'left',
-            dataAlignment: 'left',
-            sortable: true,
-            render: (_, coin) => `<a href="/coins/${coin.id}">${coin.getFullTitle()}</a>`,
-        }, {
-            title: 'Mintage',
-            dataKey: 'mintage',
-            headingAlignment: 'right',
-            dataAlignment: 'right',
-            footAlignment: 'right',
-            sortable: true,
-            render: (_, coin) => coin.getMintageString(),
-            renderFoot: (data) => Coin.getMintageString(data.reduce((prevMintage, currCoin) => prevMintage + (currCoin.mintage || 0), 0)),
-        }, {
-            title: 'Diameter',
-            dataKey: 'diameter',
-            headingAlignment: 'right',
-            dataAlignment: 'right',
-            sortable: true,
-            render: (_, coin) => coin.getDiameterString(),
-        }, {
-            title: 'Weight',
-            dataKey: 'composition.weight',
-            headingAlignment: 'right',
-            dataAlignment: 'right',
-            sortable: true,
-            render: (_, coin) => coin.getWeightString(),
-        }, {
-            title: 'Add',
-            dataKey: 'id',
-            headingAlignment: 'center',
-            dataAlignment: 'center',
-            sortable: false,
-            render: (_, coin) => $session.user ? `<button><img src="/icons/plus-circle.svg" alt="Add Coin" width="16" height="16" /></button>` : '',
-        }
-    ]}
-    data={coins}
-    ></Table>
-</BaseLayout>
+<Table
+pageSizes={[5, 10, 25, 50, 100]}
+columns={[{
+        title: 'Strike',
+        dataKey: 'strike.title',
+        headingAlignment: 'left',
+        dataAlignment: 'left',
+        sortable: true,
+    }, {
+        title: 'Year',
+        dataKey: 'year',
+        headingAlignment: 'left',
+        dataAlignment: 'left',
+        sortable: true,
+        render: (_, coin) => `<a href="/coins/${coin.id}">${coin.getFullTitle()}</a>`,
+    }, {
+        title: 'Mintage',
+        dataKey: 'mintage',
+        headingAlignment: 'right',
+        dataAlignment: 'right',
+        footAlignment: 'right',
+        sortable: true,
+        render: (_, coin) => coin.getMintageString(),
+        renderFoot: (data) => Coin.getMintageString(data.reduce((prevMintage, currCoin) => prevMintage + (currCoin.mintage || 0), 0)),
+    }, {
+        title: 'Diameter',
+        dataKey: 'diameter',
+        headingAlignment: 'right',
+        dataAlignment: 'right',
+        sortable: true,
+        render: (_, coin) => coin.getDiameterString(),
+    }, {
+        title: 'Weight',
+        dataKey: 'composition.weight',
+        headingAlignment: 'right',
+        dataAlignment: 'right',
+        sortable: true,
+        render: (_, coin) => coin.getWeightString(),
+    }, {
+        title: 'Add',
+        dataKey: 'id',
+        headingAlignment: 'center',
+        dataAlignment: 'center',
+        sortable: false,
+        render: (_, coin) => $session.user ? `<button><img src="/icons/plus-circle.svg" alt="Add Coin" width="16" height="16" /></button>` : '',
+    }
+]}
+data={coins}
+></Table>
 
 <style lang="scss">
     .toggle-controls {

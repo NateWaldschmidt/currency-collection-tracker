69da06edce4d0c03c7dc836d59f56fe370943a1a
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-09-30 19:44:37 -0500

    Remove the param constraint for letters first in alphanum params

--- src/params/alphanum.ts
+++ src/params/alphanum.ts
@@ -2,5 +2,5 @@ import type { ParamMatcher } from "@sveltejs/kit";
 
 /** Matches routes that are alpha numeric with underscores. */
 export const match: ParamMatcher = function(param) {
-    return /^[a-zA-Z][a-zA-Z0-9\-]*$/.test(param);
+    return /^[a-zA-Z0-9\-]*$/.test(param);
 }

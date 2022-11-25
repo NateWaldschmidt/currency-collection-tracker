e12f7b25c4a2aa2ae7a8a66192e70cd50f0f314c
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-11-25 16:45:26 -0600

    Adjust coin strike repo to use base entity and add validation constraints

--- src/lib/models/coin-strike.ts
+++ src/lib/models/coin-strike.ts
@@ -1,8 +1,35 @@
-/** The model for a coin strike. */
-export default class CoinStrike {
-    public id?: number;
+import Entity from "$lib/models/entity";
+import type { ObjectSchema } from "joi";
+import Joi from "joi";
+
+export interface CoinStrikeData {
+    id: number,
     /** The title of the strike. */
-    public title?: string;
+    title: string,
     /** An abbreviation of the strike title. */
-    public abbreviation?: string;
+    abbreviation?: string,
 }
+
+interface CoinStrike extends CoinStrikeData { }
+
+class CoinStrike extends Entity<CoinStrike, CoinStrikeData> {
+
+    /** @inheritDoc */
+    public getSchema(): ObjectSchema<CoinStrike> {
+        return Joi.object({
+            id:           Joi.number()
+                             .integer()
+                             .min(1)
+                             .required(),
+            title:        Joi.string()
+                             .min(1)
+                             .max(255),
+            abbreviation: Joi.string()
+                             .min(1)
+                             .max(255)
+                             .allow(null),
+        });
+    }
+}
+
+export default CoinStrike;

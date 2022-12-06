e82b6de5e193d2820e92c4e2d7a3802165581384
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-12-06 08:58:12 -0600

    Adjust Grading Company API endpoint to return proper responses

--- src/routes/api/grading-companies/[companyUrlKey=alphanum]/+server.ts
+++ src/routes/api/grading-companies/[companyUrlKey=alphanum]/+server.ts
@@ -1,23 +1,22 @@
-import createConnection from "$lib/server/database/connection";
-import GradingCompanyRepository from "$lib/server/repository/grading-company-repository";
+import GradingCompany from "$lib/entities/coins/grading-company.entity";
 import ResponseHelper from "$lib/server/utilities/response-helper";
 import type { RequestHandler } from "@sveltejs/kit/types/internal";
 
 /** Finds a particular grading company by ID. */
 export const GET: RequestHandler = async function({ locals, params }) {
-    const gradingCompanyRepo = new GradingCompanyRepository(locals.connection);
-    const gradingCompany = await gradingCompanyRepo.findByUrlKey(params.companyUrlKey);
+    const gradingCompanyRepo = locals.dataSource.getRepository(GradingCompany);
+    const gradingCompany = await gradingCompanyRepo.findOneBy({ urlKey: params.companyUrlKey });
 
     // Did not find a grading company with that ID.
     if (!gradingCompany) {
-        return new Response(null, {
-            status: 404,
-            statusText: `Could not find a grading company with the URL key of ${params.companyUrlKey}.`,
-        })
+        return ResponseHelper.jsonResponse(
+            { message: 'Could not find a grading company with the URL key of ${params.companyUrlKey}.' },
+            404
+        );
     }
 
     return ResponseHelper.jsonResponse(
-        `Successfully queried for the grading company.`,
-        gradingCompany,
+        { message: 'Successfully queried for the grading company.', data: gradingCompany },
+        200,
     );
 }
